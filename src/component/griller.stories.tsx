5c28d513cf5b78925227fdd2602db84b00f3d5f3
marius <marius.ahsmus@gmail.com>
2024-07-26 16:22:36 +0200

    feat: animation, closeButton, positioning, ref

--- src/component/griller.stories.tsx
+++ src/component/griller.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Griller} from './griller';
+import React, { useRef } from 'react';
+import {Griller, GrillerRef} from './griller';
 import {Meta, StoryObj} from "@storybook/react";
 import {TriangleAlert} from "lucide-react";
 
@@ -18,11 +18,23 @@ type Story = StoryObj<typeof Griller>
 
 export const Default: Story = {
     render: () => {
+        const grillerRef = useRef<GrillerRef>(null);
+
         return (
-            <Griller title={"Alert Title"}
-                     secondTitle={"This is a second title"}
-                     icon={<TriangleAlert size={24} className={"text-zinc-700"}/>}
-            />
+            <>
+                <button title={"Button"}
+                        className={"bg-amber-600"}
+                        onClick={() => grillerRef.current?.show()}
+                />
+
+                <Griller title={"Alert Title"}
+                         secondTitle={"This is a second title"}
+                         icon={<TriangleAlert size={24} className={"text-zinc-700"}/>}
+                         placement={"tr"}
+                         closeButton={true}
+                         ref={grillerRef}
+                />
+            </>
         );
     },
 };
--- src/component/griller.tsx
+++ src/component/griller.tsx
@@ -1,23 +1,108 @@
-import React, {HTMLAttributes, ReactNode} from "react";
+"use client";
+
+import React, {forwardRef, HTMLAttributes, ReactNode, useEffect, useImperativeHandle, useRef, useState} from "react";
+import {cn} from "@/lib/utlis";
+import {X} from "lucide-react";
 
 interface GrillerProps extends HTMLAttributes<HTMLDivElement> {
     title: string;
     secondTitle?: string;
     icon?: ReactNode;
     placement?: "tr" | "tl" | "br" | "bl";
+    closeButton?: boolean;
+    duration?: number;
+    closeSize?: number;
+    titleClassname?: string;
+    secondTitleClassname?: string;
+    closeClassname?: string;
 }
 
-export const Griller: React.FC<GrillerProps> = ({ title, secondTitle, icon, className, ...props }) => {
+type GrillerRef =  {
+    show: () => void;
+    hide: () => void;
+    current: HTMLDivElement | null;
+};
+
+const Griller = forwardRef<GrillerRef, GrillerProps>(({ title, secondTitle, icon, placement, closeButton, duration, closeSize,
+                                                    titleClassname, secondTitleClassname, closeClassname, className, ...props }, ref) => {
+    const [visible, setVisible] = useState(false);
+    const [animate, setAnimate] = useState(false);
+    const grillerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        let showTimeout: NodeJS.Timeout, hideTimeout: NodeJS.Timeout;
+        if (visible) {
+            setAnimate(true);
+            showTimeout = setTimeout(() => {
+                if (grillerRef.current) {
+                    grillerRef.current.classList.add('opacity-100', 'translate-y-0');
+                    grillerRef.current.classList.remove('opacity-0', 'translate-y-full');
+                }
+            }, 10);
+            if (duration) {
+                hideTimeout = setTimeout(() => {
+                    if (grillerRef.current) {
+                        grillerRef.current.classList.remove('opacity-100', 'translate-y-0');
+                        grillerRef.current.classList.add('opacity-0', 'translate-y-full');
+                    }
+                    hideTimeout = setTimeout(() => {
+                        setVisible(false);
+                        setAnimate(false);
+                    }, 500);
+                }, duration);
+            }
+        }
+        return () => {
+            clearTimeout(showTimeout);
+            clearTimeout(hideTimeout);
+        };
+    }, [visible, duration]);
+
+    useImperativeHandle(ref, () => ({
+        show: () =>  {
+            setVisible(true);
+            setAnimate(true);
+        },
+        hide: () => {
+            setVisible(false);
+            setVisible(false);
+        },
+        current: grillerRef.current,
+    }));
 
     return (
-        <div className={"fixed bottom-4 right-4 z-50 w-72 h-16 flex flex-row items-center space-x-4 px-4 py-2 " +
-            "bg-zinc-200 rounded-lg"} {...props}
-        >
-            {icon}
-            <div className={"flex flex-col space-y-0.5"}>
-                <span className={"text-sm text-zinc-700 font-medium"}>{title}</span>
-                {secondTitle && <span className={"text-xs text-zinc-500"}>{secondTitle}</span>}
-            </div>
-        </div>
+        <>
+            {animate &&
+                <div className={cn("fixed z-50 shadow-lg", {
+                    "bottom-4 right-4": !placement || placement === "br",
+                    "top-4 right-4": placement === "tr",
+                    "top-4 left-4": placement === "tl",
+                    "bottom-4 left-4": placement === "bl",
+                })}>
+
+                    <div className={cn("w-72 h-16 flex flex-row justify-between p-2 pl-4 bg-zinc-200 rounded-lg",className)} ref={grillerRef} {...props}>
+                        <div className={"flex flex-row items-center space-x-4"}>
+                            {icon}
+                            <div className={"flex flex-col"}>
+                                <span className={cn("text-sm text-zinc-700 font-medium", titleClassname)}>{title}</span>
+                                {secondTitle && <span className={cn("text-xs text-zinc-500", secondTitleClassname)}>{secondTitle}</span>}
+                            </div>
+                        </div>
+                        {closeButton &&
+                            <div className={"h-max p-0.5 rounded-lg cursor-pointer hover:bg-zinc-300"}
+                                 onClick={() => {setVisible(false); setAnimate(false);}}
+                            >
+                                <X size={closeSize ?? 16} className={cn("text-zinc-500", closeClassname)}/>
+                            </div>
+                        }
+                    </div>
+                </div>
+            }
+        </>
     );
-}
+});
+Griller.displayName = "Griller";
+
+export {Griller};
+export type { GrillerRef };
+
