125e9ac367e39e0a4a9de946e96075da2d242342
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-07-30 18:16:25 +0200

    feat: copyButton animation, copy logic fixed

--- src/lib/copybutton.tsx
+++ src/lib/copybutton.tsx
@@ -2,6 +2,7 @@
 
 import React, {HTMLAttributes, useEffect, useState} from "react";
 import {Check, Clipboard} from "lucide-react";
+import {AnimatePresence, motion} from "framer-motion";
 
 interface CopyButtonProps extends HTMLAttributes<HTMLDivElement> {
     copyText: string;
@@ -11,17 +12,44 @@ const CopyButton: React.FC<CopyButtonProps> = ({ copyText, className, ...props }
     const [isChecked, setIsChecked] = useState(false);
 
     useEffect(() => {
-        setTimeout(() => setIsChecked(false), 3000);
+        const timer = setTimeout(() => setIsChecked(false), 5000);
+        return () => clearTimeout(timer);
     }, [isChecked]);
 
     return (
         <button className={"h-max bg-zinc-100 border border-zinc-200 p-1 rounded-lg text-zinc-500"}
                 onClick={() => {
                     setIsChecked(true);
-                    navigator.clipboard.writeText(copyText);
+                    navigator.clipboard.writeText(copyText).then(() => {
+                        console.log("Copied");
+                    }).catch(err => {
+                        console.error('Fehler beim Kopieren in die Zwischenablage:', err);
+                    });
                 }}
         >
-            {isChecked ? <Check size={14}/> : <Clipboard size={14}/>}
+            <AnimatePresence mode={"wait"}>
+                {isChecked ? (
+                    <motion.div
+                        key="check"
+                        initial={{ opacity: 0, scale: 0.4}}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.4, y: '100%'}}
+                        transition={{ duration: 0.2 }}
+                    >
+                        <Check size={14} />
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        key="clipboard"
+                        initial={{ opacity: 0, scale: 0.4 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.4, y: '100%' }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        <Clipboard size={14} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </button>
     );
 }
