31aebe21a11301308fcf384fd4a97dc4f134a414
marius <marius.ahsmus@gmail.com>
2024-08-12 13:00:17 +0200

    feat: added toast-portal

--- src/component/toast.tsx
+++ src/component/toast.tsx
@@ -5,6 +5,7 @@ import {X} from "lucide-react";
 import {AnimatePresence, motion} from "framer-motion";
 import clsx, {ClassValue} from "clsx";
 import {twMerge} from "tailwind-merge";
+import ReactDOM from "react-dom";
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes));
 
@@ -19,6 +20,10 @@ export const positionClasses = (position: Position) => {
     if (position === 'bl') return 'bottom-4 left-4';
     if (position === 'bc') return 'bottom-4 left-1/2';
     return '';
+}
+
+const ToastPortal: React.FC<{ children: ReactNode }> = ({ children }) => {
+    return ReactDOM.createPortal(children, document.body);
 }
 
 interface ToastProps extends HTMLAttributes<HTMLDivElement> {
@@ -46,10 +51,10 @@ const Toast: React.FC<ToastProps & {
     removeToast: (id: string) => void,
     isPaused: boolean,
 }> = ({
-   id, title, secondTitle, icon, position = "br", closeButton,
-                       actionButton, onAction, actionButtonText, duration = 3000, theme = "light", titleClassname,
-         secondTitleClassname, closeClassname, closeDivClassname, motionClassname, iconClassname, actionButtonClassname,
-         className, removeToast, isPaused, ...props }) => {
+   id, title, secondTitle, icon, position = "br", closeButton, actionButton, onAction, actionButtonText,
+   duration = 3000, theme = "light", titleClassname, secondTitleClassname, closeClassname, closeDivClassname,
+   motionClassname, iconClassname, actionButtonClassname, className, removeToast, isPaused, ...props
+}) => {
 
     const [visible, setVisible] = useState(true);
     const [width, setWidth] = useState<number>(0);
@@ -103,74 +108,76 @@ const Toast: React.FC<ToastProps & {
     };
 
     return (
-        <AnimatePresence onExitComplete={() => removeToast(id)}>
-            {visible && (
-                <motion.div
-                    key={id}
-                    ref={toastRef}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                    variants={variants}
-                    transition={{duration: 0.5,}}
-                    className={cn("shadow-xs shadow-zinc-900 rounded-lg", positionClasses(position), motionClassname)}
-                    style={position === "tc" || position === "bc" ? { marginLeft: `-${width / 2}px` } : {}}
-                >
-                    <div
-                        className={cn("min-w-72 min-h-16 flex flex-row justify-between p-2 pl-4 rounded-lg",
-                            theme === "light" ?
-                                "bg-zinc-50 border border-zinc-200" :
-                                "bg-zinc-800 border border-zinc-700",
-                            className
-                        )}
-                        {...props}
+        <ToastPortal>
+            <AnimatePresence onExitComplete={() => removeToast(id)}>
+                {visible && (
+                    <motion.div
+                        key={id}
+                        ref={toastRef}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        variants={variants}
+                        transition={{duration: 0.5,}}
+                        className={cn("shadow-xs shadow-zinc-900 rounded-lg", positionClasses(position), motionClassname)}
+                        style={position === "tc" || position === "bc" ? { marginLeft: `-${width / 2}px` } : {}}
                     >
-                        <div className={cn("flex flex-row items-center space-x-2", closeButton && "mr-2")}>
-                            {icon && (
-                                <div className={cn(iconClassname, theme === "light" ? "text-zinc-600" : "text-zinc-100",)}>
-                                    {icon}
-                                </div>
+                        <div
+                            className={cn("min-w-72 min-h-16 flex flex-row justify-between p-2 pl-4 rounded-lg",
+                                theme === "light" ?
+                                    "bg-zinc-50 border border-zinc-200" :
+                                    "bg-zinc-800 border border-zinc-700",
+                                className
                             )}
-                            <div className={cn("flex flex-col max-w-60", icon && "ml-4", actionButton)}>
-                                <span className={cn("text-sm font-medium text-nowrap truncate",
-                                    theme === "light" ? "text-zinc-600" : "text-zinc-100",
-                                    titleClassname
+                            {...props}
+                        >
+                            <div className={cn("flex flex-row items-center space-x-2", closeButton && "mr-2")}>
+                                {icon && (
+                                    <div className={cn(iconClassname, theme === "light" ? "text-zinc-600" : "text-zinc-100",)}>
+                                        {icon}
+                                    </div>
                                 )}
-                                >
-                                    {title}
-                                </span>
-                                {secondTitle && secondTitle.trim() !== "" && (
-                                    <span className={cn("text-xs", theme === "light" ? "text-zinc-400" : "text-zinc-300", secondTitleClassname)}>
-                                        {secondTitle}
+                                <div className={cn("flex flex-col max-w-60", icon && "ml-4", actionButton)}>
+                                    <span className={cn("text-sm font-medium text-nowrap truncate",
+                                        theme === "light" ? "text-zinc-600" : "text-zinc-100",
+                                        titleClassname
+                                    )}
+                                    >
+                                        {title}
                                     </span>
-                                )}
+                                    {secondTitle && secondTitle.trim() !== "" && (
+                                        <span className={cn("text-xs", theme === "light" ? "text-zinc-400" : "text-zinc-300", secondTitleClassname)}>
+                                            {secondTitle}
+                                        </span>
+                                    )}
+                                </div>
+                                {actionButton &&
+                                    <button className={cn("h-max py-1 px-2 rounded-lg text-xs",
+                                        theme === "light" ?
+                                            "bg-zinc-100 border border-zinc-200 text-zinc-500 hover:bg-zinc-200" :
+                                            "bg-zinc-800 border border-zinc-700 text-zinc-300 hover:bg-zinc-700",
+                                        actionButtonClassname
+                                    )}
+                                            onClick={onAction}>
+                                        {actionButtonText || "Undo"}
+                                    </button>
+                                }
                             </div>
-                            {actionButton &&
-                                <button className={cn("h-max py-1 px-2 rounded-lg text-xs",
-                                    theme === "light" ?
-                                        "bg-zinc-100 border border-zinc-200 text-zinc-500 hover:bg-zinc-200" :
-                                        "bg-zinc-800 border border-zinc-700 text-zinc-300 hover:bg-zinc-700",
-                                    actionButtonClassname
+                            {closeButton &&
+                                <div className={cn("h-max p-0.5 rounded-lg cursor-pointer",
+                                    theme === "light" ? "hover:bg-zinc-200" : "hover:bg-zinc-700",
+                                    closeDivClassname
                                 )}
-                                        onClick={onAction}>
-                                    {actionButtonText || "Undo"}
-                                </button>
+                                     onClick={() => setVisible(false)}
+                                >
+                                    <X size={16} className={cn(theme === "light" ? "text-zinc-500" : "text-zinc-400", closeClassname)}/>
+                                </div>
                             }
                         </div>
-                        {closeButton &&
-                            <div className={cn("h-max p-0.5 rounded-lg cursor-pointer",
-                                theme === "light" ? "hover:bg-zinc-200" : "hover:bg-zinc-700",
-                                closeDivClassname
-                            )}
-                                 onClick={() => setVisible(false)}
-                            >
-                                <X size={16} className={cn(theme === "light" ? "text-zinc-500" : "text-zinc-400", closeClassname)}/>
-                            </div>
-                        }
-                    </div>
-                </motion.div>
-            )}
-        </AnimatePresence>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </ToastPortal>
     );
 };
 
